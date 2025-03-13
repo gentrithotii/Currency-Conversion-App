4b0f26d157c7cb8f3467f222cf99a6e589baec47
Gentrithotii <Gentrithoti@hotmail.com>
2025-03-13 10:42:29 +0100

    Menu setup

---
+++ src/main/java/org/example/Menu/Menu.java
@@ -0,0 +1,53 @@
+package org.example.Menu;
+
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner sc;
+
+    public Menu() {
+        sc = new Scanner(System.in);
+    }
+
+    public void printOutMainMenu() {
+        while (true) {
+            System.out.println("Currency Converter App");
+
+            System.out.println(" ");
+
+            System.out.println("1. Convert SEK to USD");
+            System.out.println("2. Convert USD to SEK");
+            System.out.println("3. Convert SEK to Euro");
+            System.out.println("4. Convert Euro to SEK");
+            System.out.println("0. Exit");
+
+            System.out.println(" ");
+
+            System.out.print("Enter your choice: ");
+            int menuChoice = sc.nextInt();
+            sc.nextLine();
+
+            switch (menuChoice) {
+                case 1:
+                    System.out.println("Convert sek to usd");
+                    break;
+                case 2:
+                    System.out.println("Usd to Sek");
+                    break;
+                case 3:
+                    System.out.println("SEK to Euro");
+                    break;
+                case 4:
+                    System.out.println("Euro to SEK");
+                    break;
+                case 0:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Wrong menu choice");
+            }
+
+
+        }
+    }
+}
